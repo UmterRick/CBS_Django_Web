0e05ac09ac74103857762ab2ae4c7621edd1462b
umterrick <usichenko.vlad67@gmail.com>
2024-07-06 10:10:39 +0200

    Django routes

--- cbs_web/first_app/urls.py
+++ cbs_web/first_app/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from first_app import views
 
 urlpatterns = [
     path('lesson-1', views.index, name="index_page"),
+    path('lesson-1/hw', views.html_response, name="html_page"),
+    path('news', views.get_all_news),
+    path('news/<int:news_id>', views.get_news_by_id),  # app/news/1
+    re_path('^news/(?P<news_title>[a-zA-Z0-9]{5})', views.regexp_route),
+
 ]
--- cbs_web/first_app/views.py
+++ cbs_web/first_app/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseForbidden
 
 
 # from django.shortcuts import render
@@ -6,3 +7,29 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world ! Ви на сторінці 1 уроку.")
+
+
+def html_response(request):
+    return HttpResponse("""
+    <p><i>Hello World!</i></p>
+
+    <p><b>Django є одним з найбільших framework на Python</b></p>
+
+    <hr>
+""")
+
+
+def get_all_news(request):
+    return HttpResponse()
+
+
+def get_news_by_id(request, news_id):
+    if news_id > 100 and not request.user.is_authenticated:
+        return HttpResponseForbidden()
+
+    print(f"Get News with id={news_id}", type(news_id))
+    return HttpResponse(f"<b> News(id={news_id}) </b>")
+
+def regexp_route(request, news_title):
+    print(f"Search for news with title: {news_title}")
+    return HttpResponse()
